d922629bbbe4f55a8df3f8512cadb37136fb4b45
ottomata <otto@wikimedia.org>
2012-11-29 16:48:05 -0500

    using fqdn for analytics nodes
    Change-Id: Ia2ece3b118081d3da5ca9e6256bd1ce36af961a8

--- manifests/site.pp
+++ manifests/site.pp
@@ -216,50 +216,50 @@ node analytics_basenode {
 
 # analytics1001 is currently the only
 # Analytics node with a public IP.
-node analytics1001 inherits analytics_basenode {
+node analytics1001.wikimedia.org inherits analytics_basenode {
 	include role::analytics::public
 }
 
 # analytics1002 is Storm Master (i.e. Storm Nimbus server)
-node analytics1002 inherits analytics_basenode {
+node analytics1002.eqiad.wmnet inherits analytics_basenode {
 	include role::analytics::storm::master
 }
 
 # analytics1003 - analytics1009 are Storm Workers (i.e. Storm Supervisor servers)
-node /^analytics100[3-9]/ inherits analytics_basenode {
+node /^analytics100[3-9].eqiad.wmnet/ inherits analytics_basenode {
 	include role::analytics::storm::worker
 }
 
 # analytics1010 is Hadoop Master (i.e NameNode, JobTracker, and ResourceManager)
-node analytics1010 inherits analytics_basenode {
+node analytics1010.eqiad.wmnet inherits analytics_basenode {
 	Exec { environment => 'http_proxy=http://brewster.wikimedia.org:8080' }
 	include role::analytics::hadoop::master
 }
 
 
 # analytics1011-1020 are Hadoop Workers (i.e. NodeManager, DataNode)
-node /^analytics10(1[1-9]|20)/ inherits analytics_basenode {
+node /^analytics10(1[1-9]|20).eqiad.wmnet/ inherits analytics_basenode {
 	include role::analytics::hadoop::worker
 }
 
 # analytics1021 and analytics1022 are Kafka Brokers
-node analytics1021, analytics1022 inherits analytics_basenode {
+node analytics1021.eqiad.wmnet, analytics1022.eqiad.wmnet inherits analytics_basenode {
 	include role::analytics::kafka
 }
 
 # analytics1023, analytics1024 and anlytics1025 are Zookeeper Servers
-node analytics1023,analytics1024,analytics1025 inherits analytics_basenode {
+node analytics1023.eqiad.wmnet, analytics1024.eqiad.wmnet, analytics1025.eqiad.wmnet inherits analytics_basenode {
 	include role::analytics::zookeeper
 }
 
 # analytics1026 does not yet have a role
-node analytics1026 inherits analytics_basenode {
+node analytics1026.eqiad.wmnet inherits analytics_basenode {
 	include role::analytics
 }
 
 # analytics1027 hosts front end web interfaces
 # (e.g. Hue, Oozie, Storm UI, etc.)
-node analytics1027 inherits analytics_basenode {
+node analytics1027.eqiad.wmnet inherits analytics_basenode {
 	include role::analytics::frontend
 }
 
